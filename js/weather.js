6b0d9d8dbaa330824b785d1fcf1d1d92c40b58fb
Sotaro Sakuyama <tera_stay@SotaronoMacBook-Air.local>
2020-06-18 01:06:55 +0900

    任意の天気取得、摂氏に変換

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,21 @@
-/*実装未定:位置情報の取得の許可
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-        });
-    }
-});*/
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=9d50df53ff80f789c405eb8fc44b522e')
+  .then(response => response.json())
+  .then(data => {
+      var nameValue = data['name'];
+      var tempValue = Math.floor(data['main']['temp'] - 273.15) + '℃';
+      var descValue = data['weather'][0]['description'];
+
+      name.innerHTML = nameValue;
+      temp.innerHTML = tempValue;
+      desc.innerHTML = descValue;
+  })
+
+.catch(err => alert("Wrong city name!"))
+})
