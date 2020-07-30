f24e4a320036db79e69eb98b7b6f6720957af8ab
Sotaro Sakuyama <apmgjg124953@gmail.com>
2020-07-30 11:31:30 +0900

    apiから情報を取得途中

--- js/map.js
+++ js/map.js
@@ -1,7 +1,27 @@
+var request = new XMLHttpRequest();
+
+var name = document.querySelector('.name');
+var desc = document.querySelector('.total');
+
 $(function(){
     $("#map-container").japanMap({
       onSelect : function(data){
-        alert(data.name);
+        request.open('GET','https://covid19-japan-web-api.now.sh/api/v1/statistics',true);
+        request.responseType = 'json';
+        
+        request.onload = function () {
+            var data = this.response;
+            var nameValue = data['name_ja'];
+            var totalValue = data['total_count'];
+            
+            name.innerHTML = nameValue;
+            total.innerHTML = totalValue;
+        };
+
+
+        
+
+        
       }
     });
   });
