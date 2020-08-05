7ee603b57edb91733bddda10c411aee9f9dbd829
Sotaro Sakuyama <apmgjg124953@gmail.com>
2020-08-05 09:03:39 +0900

    Getpi

--- js/map.js
+++ js/map.js
@@ -1,23 +1,36 @@
-$(function(){
+var request = new XMLHttpRequest();
+
+  
+  $(function(){
     $("#map-container").japanMap({
       onSelect : function(data){
+        request.open('GET','https://covid19-japan-web-api.now.sh/api/v1/prefectures',true);
+        request.responseType = 'json';
+          
+        request.onload = function () {
+            var array = this.response;
+            console.log(array);
+            searchID = data.code;
+            target = array.filter(function(object){
+                return object.id == searchID;
+            }).shift()
+            console.log(target)
+            console.log(target.name_ja)    
+            var p_name = document.getElementById('name');
+            p_name.innerHTML = '検索対象 : '+target.name_ja;
+            var cases_n = document.getElementById('cases');
+            cases_n.innerHTML = '感染者数 : '+target.cases+ '人';
+            var pcr_n = document.getElementById('PCR');
+            pcr_n.innerHTML = 'PCR検査数 : '+target.pcr+ '件';
+        };
+
         
+        
+        
+        request.send();
+
       }
     });
   });
 
-  /*var request = new XMLHttpRequest();
-
-  var name = document.querySelector('.name'); 
-  var total = document.querySelector('.total');
-  request.open('GET','https://covid19-japan-web-api.now.sh/api/v1/statistics',true);
-  request.responseType = 'json';
-  
-  request.onload = function () {
-      var data = this.response;
-      var nameValue = data['name_ja'];
-      var totalValue = data['total_count'];
-
-      name.innerHTML = nameValue;
-      total.innerHTML = totalValue;
-  }; */
+ 
